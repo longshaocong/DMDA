87ae229e590ff85ebe585a3d274ab8394c71e559
Lsc-cs <3227034894@qq.com>
2022-09-28 09:39:19 +0800

    updata the util

---
+++ utils/util.py
@@ -0,0 +1,17 @@
+import os
+import torch
+
+# seed setting with seed 3407
+def set_random_seed(seed=3407):
+    os.environ['PYTHONHASHSEED'] = str[seed]
+
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+    np.random.seed(seed)
+    random.seed(seed)
+
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.enabled = False
