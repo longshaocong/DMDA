6fe00ecb9f466c122456414bf9a8da2ad300d4a8
Lsc-cs <3227034894@qq.com>
2022-10-01 17:39:02 +0800

    update

--- datautil/Test.py
+++ datautil/Test.py
@@ -1,9 +1,41 @@
-def foo():
-    batch = iter([1, 2, 3])
-    while True:
-        for b in batch:
-            yield b 
-            print('res:')
-g = foo()
+# def foo():
+#     batch = iter([1, 2, 3])
+#     while True:
+#         for b in batch:
+#             yield b 
+#             print('res:')
+# g = foo()
+# for i in range(20):
+#     print(next(g)) 
+import torch
+class _InfiniteSampler(torch.utils.data.Sampler):
+    """Wraps another Sampler to yield an infinite stream."""
+
+    def __init__(self, sampler):
+        self.sampler = sampler
+
+    def __iter__(self):
+        while True:
+            for batch in self.sampler:
+                yield batch
+
+
+x = [0, 1, 2, 3, 4]
+sampler = torch.utils.data.RandomSampler(x, replacement=False)
+for i in sampler:
+    print(i)
+print('-----------------')
+for i in sampler:
+    print(i)
+batch_sampler = torch.utils.data.BatchSampler(sampler, batch_size=2, drop_last=False)
+for i in batch_sampler:
+    print(i)
+print('===================')
+for i in batch_sampler:
+    print(i)
+print('===================')
+new_batch_sampler = iter(_InfiniteSampler(batch_sampler))
 for i in range(20):
-    print(next(g)) 
+    print(next(new_batch_sampler))
+
+
